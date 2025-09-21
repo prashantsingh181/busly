ebfeccb16eb477d7a12985145904296bd872c54f
prashantsingh181 <prashant.singh181@gmail.com>
2025-09-21 23:04:49 +0530

    feat: added demo tickets

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -3,3 +3,10 @@ import { isToday, isBefore, startOfDay } from "date-fns";
 export function isTodayOrBefore(date: string) {
   return isToday(date) || isBefore(date, startOfDay(new Date()));
 }
+
+export function generateDate(daysAhead: number, hour: number = 8) {
+  const d = new Date();
+  d.setDate(d.getDate() + daysAhead);
+  d.setHours(hour, 0, 0, 0);
+  return d.toISOString();
+}
