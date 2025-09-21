a0f6e9050dba954f395878ff4cc46dcf504a34b1
prashantsingh181 <prashant.singh181@gmail.com>
2025-09-21 23:08:18 +0530

    fix: classNamemistake

--- src/pages/book/components/Seat.tsx
+++ src/pages/book/components/Seat.tsx
@@ -16,9 +16,9 @@ export default function Seat({
   const seatClassName =
     "w-[3rem] sm:w-[3.25rem] border-2 border-neutral-500 bg-neutral-100 text-neutral-700 rounded-lg grid place-items-center aspect-square hover:bg-theme-50 hover:border-theme-400 hover:text-theme-600 transition-colors";
   const selectedSeatClassName =
-    "w-[3rem] w-[3.25rem] border-2 border-theme-500 bg-theme-100 text-theme-700 rounded-lg grid place-items-center aspect-square relative";
+    "w-[3rem] sm:w-[3.25rem] border-2 border-theme-500 bg-theme-100 text-theme-700 rounded-lg grid place-items-center aspect-square relative";
   const soldSeatClassName =
-    "w-[3rem] w-[3.25rem] border-2 border-neutral-200 bg-neutral-300 text-neutral-700 rounded-lg grid place-items-center aspect-square cursor-not-allowed relative";
+    "w-[3rem] sm:w-[3.25rem] border-2 border-neutral-200 bg-neutral-300 text-neutral-700 rounded-lg grid place-items-center aspect-square cursor-not-allowed relative";
   return (
     <button
       className={
